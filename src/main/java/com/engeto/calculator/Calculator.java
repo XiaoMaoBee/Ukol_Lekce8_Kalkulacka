1ff492bb9ff261332a15014c992372ed1c84d2a2
OldrichLapka <Oldrich.Lapka@kyndryl.com>
2023-03-27 19:30:12 +0200

    Initial Commit Lekce8 úkol

---
+++ src/main/java/com/engeto/calculator/Calculator.java
@@ -0,0 +1,39 @@
+package com.engeto.calculator;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        return (double)a / b;
+    }
+    public double squareRoot(int a) {
+        if (a < 0) {
+            throw new ArithmeticException("Negative value not allowed");
+        }
+        return Math.sqrt(a);
+    }
+
+    public double square(int a) {
+        return a * a;
+    }
+
+    public double areaOfCircle(int a) {
+        if (a < 0) {
+            throw new ArithmeticException("Negative value not allowed");
+        }
+        return (Math.PI * ((a*a)));
+    }
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.engeto.calculator.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Calculator calculator = new Calculator();
+        calculator.squareRoot(-15);
+    }
+}
